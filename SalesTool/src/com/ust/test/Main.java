3b324073abe8df95b21957091dbc143991a30e19
user55 <user55@user55-desktop>
2019-09-24 14:54:48 +0530

    initial commit by athira

---
+++ SalesTool/src/com/ust/test/Main.java
@@ -0,0 +1,18 @@
+package com.ust.test;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData objSalesData=new SalesData();
+		System.out.println("welcomr to sales app");
+		displayGreeting();
+		//objSalesData.display();
+		
+		
+		
+	}
+	public static void displayGreeting(){
+		System.out.println("succesfully");
+		
+	}
+
+}
